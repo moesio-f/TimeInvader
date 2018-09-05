76c8df02930bb985c647aa709fd626a1c0c58d7a
moesio-f <moesiof.dev@gmail.com>
2018-09-05 14:03:49 -0300

    Ínicio - Escada
    Código base para a produção da Escada;
    Código com erro;

---
+++ Assets/Scripts/LadderFunction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class LadderFunction : MonoBehaviour {
+	
+
+	public float Speed;
+	
+	bool PressW;
+	
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+	}
+
+	void OnTriggerStay2D()
+	{
+		if(other.tag=="Player" && Input.GetButtonDown("Jump"))
+		{
+
+		}
+	}
+}
